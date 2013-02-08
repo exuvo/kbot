466f7d9696c77d752e5818a1166f437fd428f2cb
exuvo <exuvo@exuvo.se>
2013-02-09 00:48:58 +0100

    renamed libs to lib.

--- README.md
+++
@@ -1,4 +0,0 @@
-kbotpi
-======
-
-Kartobot RaspberryPi
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "config.h"
+#include <string.h>
 
 int main( int argc, const char* argv[] ){
 	printf("kbotpi version %u.%u\n", kbotpi_VERSION_MAJOR, kbotpi_VERSION_MINOR);
+  
+  for(int i=0; i < argc; i++){
+		if(strcmp(argv[i], "--help")){
 
+		}
+
+	
+	}
 }
