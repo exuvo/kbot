3a874bd36cead4368e351d6de138731531383b0d
exuvo <exuvo@exuvo.se>
2013-09-05 14:57:33 +0200

    Added setting types classes and templates

--- kbot_tui/src/ui/settings.h
+++ kbot_tui/src/ui/settings.h
@@ -2,15 +2,45 @@
 #define SETTINGS_H
 
 #include "ui.h"
+#include <boost/any.hpp>
+
+template <class T> class settingValue{
+private:
+public:
+	string desc;
+	T value;
+};
 
 class setting {
 private:
-	string name, desc;
+	string name, desc, ROS_URL;
+	function<void()> callback;
 public:
-	setting(string name, string description, initializer_list<settingValues>);
-	setting(string name, string description, initializer_list<setting> subList);
+	setting(string name, string description, string ROS_URL);
+  setting(string name, string description, string ROS_URL, function<void()> apply);
   ~setting();
-}
+	virtual bool input(int key);
+	virtual void load();
+};
+
+template <typename T> class settingChoice : setting{
+private:
+	settingValue<T> *value;
+  vector<settingValue<T>> values;	
+public:
+	settingChoice(string name, string description, string ROS_URL, initializer_list<settingValue<T>>, function<void()> apply);
+  ~settingChoice();
+	bool input(int key);
+};
+
+template <typename T> class settingNum: setting{
+private:
+	int value, min, max;
+public:
+  settingNum(string name, string description, string ROS_URL, int min, int max, function<void()> apply);
+	~settingNum();
+	bool input(int key);
+};
 
 class settingsTab : public tab {
 private:
